639d6abd228806fd1d70b00e2237a74ab9ce2f78
Gnusin Pavel <pavel.sin.gnusin@gmail.com>
2020-07-30 22:08:05 +0300

    #44 remove redundant check

--- github/repo_delete.go
+++ github/repo_delete.go
@@ -5,7 +5,7 @@ import (
 	"github.com/g4s8/gitstrap/context"
 	"log"
 )
-
+// @todo #44:30min Remove unused variable
 var errNoRepoName = errors.New("no repo name config")
 
 // Delete GitHub repo
@@ -15,9 +15,6 @@ func (r *Repo) Delete(ctx *context.Context) error {
 		return err
 	}
 	owner := ctx.Owner()
-	if err != nil {
-		return err
-	}
 	if _, err := ctx.Client.Repositories.Delete(ctx.Sync, owner, name); err != nil {
 		return err
 	}
