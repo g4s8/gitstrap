f956b79b35732fc7db8641dd5c86bc2cf2a08ec2
Kirill <g4s8.public@gmail.com>
2021-03-27 14:02:07 +0300

    Apply, create and delete multiple documents
    For #52 - parse multiple models from one yaml file,
    common decorator for apply, create and delete command
    for working with spec models.

--- internal/spec/model_reader.go
+++ internal/spec/model_reader.go
@@ -2,17 +2,28 @@ package spec
 
 import (
 	"bufio"
+	"errors"
 	"gopkg.in/yaml.v3"
 	"io"
 	"os"
 	"path/filepath"
 )
 
+var errReadNoDocument = errors.New("Not YAML document")
+
 // FromDecoder creates model from yaml decoder
 func (m *Model) FromDecoder(d *yaml.Decoder) error {
-	if err := d.Decode(m); err != nil {
+	type Doc struct {
+		Model `yaml:"inline"`
+	}
+	doc := new(Doc)
+	if err := d.Decode(&doc); err != nil {
 		return err
 	}
+	if d == nil {
+		return errReadNoDocument
+	}
+	*m = doc.Model
 	if m.Metadata == nil {
 		m.Metadata = new(Metadata)
 	}
@@ -24,16 +35,36 @@ func (m *Model) FromReader(r io.Reader) error {
 	return m.FromDecoder(yaml.NewDecoder(r))
 }
 
-// FromFile creates model from file
-func (m *Model) FromFile(name string) error {
+// ReadFile models from file
+func ReadFile(name string) ([]*Model, error) {
 	fn, err := filepath.Abs(name)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	f, err := os.Open(fn)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer f.Close()
-	return m.FromReader(bufio.NewReader(f))
+	return ReadStream(bufio.NewReader(f))
+}
+
+// ReadStream of models from reader
+func ReadStream(r io.Reader) ([]*Model, error) {
+	dec := yaml.NewDecoder(r)
+	res := make([]*Model, 0)
+	for {
+		model := new(Model)
+		err := model.FromDecoder(dec)
+		if err == nil {
+			res = append(res, model)
+		} else if errors.Is(err, errReadNoDocument) {
+			continue
+		} else if errors.Is(err, io.EOF) {
+			break
+		} else {
+			return nil, err
+		}
+	}
+	return res, nil
 }
